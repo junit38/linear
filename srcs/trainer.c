aba92b63d749232a7dc0a08f4fcccff853d6261b
Junit38 <Junit38@gmail.com>
2020-11-14 15:04:55 +0100

    norm

--- srcs/trainer.c
+++ srcs/trainer.c
@@ -6,7 +6,7 @@
 /*   By: mery <mery@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/01/15 15:43:46 by jmery             #+#    #+#             */
-/*   Updated: 2020/10/05 14:29:59 by mery             ###   ########.fr       */
+/*   Updated: 2020/11/14 15:04:41 by mery             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,35 +22,16 @@ int				check_file(int fd)
 	lseek(fd, 0, SEEK_SET);
 	while (get_next_line(fd, &line) != 0 && is_formated == 1)
 	{
-		if (ft_isdigit(line[0]))
+		table = ft_strsplit(line, ',');
+		if (table)
 		{
-			table = ft_strsplit(line, ',');
-			if (table)
-			{
-				if (!table[0] || !table[1])
-					is_formated = 0;
-				free_table(table);
-			}
+			if (!table[0] || !table[1])
+				is_formated = 0;
+			free_table(table);
 		}
 		free(line);
 	}
 	return (is_formated);
-}
-
-void		get_abscisse(int fd, t_data *data)
-{
-	int		asc;
-	int		desc;
-
-	asc = get_asc_set(fd);
-	desc = get_desc_set(fd);
-	if (asc > desc)
-	{
-		data->asc = -1;
-		data->theta0 = get_max_set(fd);
-	}
-	else
-		data->theta0 = get_min_set(fd);
 }
 
 void		train_model(char *file_data, t_data *data)
@@ -62,11 +43,12 @@ void		train_model(char *file_data, t_data *data)
 		fd = open(file_data, O_RDONLY);
 		if (fd != -1)
 		{
-			get_abscisse(fd, data);
-			data->xmax = get_normalize_max(fd);
-			data->xmin = get_normalize_min(fd);
 			if (check_file(fd))
+			{
+				data->xmax = get_normalize_max(fd);
+				data->xmin = get_normalize_min(fd);
 				training_on_set(fd, data);
+			}
 			else
 				ft_putstr("File badly formated\n");
 			close(fd);
--- srcs/training.c
+++ srcs/training.c
@@ -6,7 +6,7 @@
 /*   By: mery <mery@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/01/15 15:43:46 by jmery             #+#    #+#             */
-/*   Updated: 2020/10/05 14:31:12 by mery             ###   ########.fr       */
+/*   Updated: 2020/11/14 15:04:19 by mery             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,8 +38,8 @@ static double	get_s2(char *line, t_data *data)
 	if (table)
 	{
 		if (table[0] && table[1])
-			s2 = (get_estimated_price(ft_atoi(table[0]), data)
-			- ft_atoi(table[1])) * get_normalize_val(ft_atoi(table[0]), data);
+			s2 = (get_normalize_val(get_estimated_price(ft_atoi(table[0]), data), data)
+			- get_normalize_val(ft_atoi(table[1]), data)) * get_normalize_val(ft_atoi(table[0]), data);
 		free_table(table);
 	}
 	return (s2);
@@ -57,7 +57,7 @@ static void		training(int fd, t_data *data)
 	s2 = 0;
 	while (get_next_line(fd, &line) != 0)
 	{
-		if (ft_strcmp(line, "km,price") != 0)
+		if (ft_isdigit(line[0]))
 		{
 			s1 += get_s1(line, data);
 			s2 += get_s2(line, data);
@@ -69,51 +69,30 @@ static void		training(int fd, t_data *data)
 	data->s2 = s2 / m;
 }
 
-static int		training_on_set_2(int fd, t_data *data)
+void		training_on_set(int fd, t_data *data)
 {
+	double	mins1;
 	double	mins2;
-	int		stop2;
+	int		stop;
 	int		iteration;
 
-	stop2 = 0;
+	stop = 0;
 	mins2 = 0;
+	mins1 = 0;
 	iteration = 0;
-	while (stop2 == 0)
+	while (stop == 0 && iteration < 1000)
 	{
 		lseek(fd, 0, SEEK_SET);
 		training(fd, data);
-		if (abs_rounded(data->s2) == mins2 && data->s2 != 0
-			&& data->theta1 != 0)
-			stop2 = 1;
+		if (abs_rounded(data->s2) - mins2 == 0 && data->s2 != 0
+			&& (abs_rounded(data->s1) - mins1) == 0 && data->s1 != 0)
+			stop = 1;
 		else
 		{
 			mins2 = abs_rounded(data->s2);
-			data->theta1 = data->theta1 - (0.00001 * data->s2);
-		}
-		iteration++;
-	}
-	return (iteration);
-}
-
-void			training_on_set(int fd, t_data *data)
-{
-	double	mins1;
-	int		stop1;
-	int		iteration;
-
-	mins1 = 0;
-	stop1 = 0;
-	iteration = 0;
-	while (stop1 == 0 && iteration < 8000)
-	{
-		data->theta1 = 0;
-		iteration += training_on_set_2(fd, data);
-		if (abs_rounded(data->s1) == mins1 && data->s1 != 0)
-			stop1 = 1;
-		else
-		{
 			mins1 = abs_rounded(data->s1);
-			data->theta0 = data->theta0 - (0.00001 * data->s1);
+			data->theta0 = data->theta0 - (1 * data->s1);
+			data->theta1 = data->theta1 - (1 * data->s2);
 		}
 		iteration++;
 	}
